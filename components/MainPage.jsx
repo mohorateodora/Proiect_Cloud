b236b7569d242630a8c7f3bc2775189c21c575c1
mohorateodora <teodoramohora2000@gmail.com>
2024-05-10 09:26:34 +0300

    modif

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -43,20 +43,33 @@ const MainPage = () => {
     useEffect(() => { fetchRecords(); }, []);
 
 
-    if (isLoading) return <Spinner />;
+    //if (isLoading) return <Spinner />;
 
 
     return (
+        <section>
+        <h1 className={"w-[500px] mx-auto text-center mt-3 text-6xl font-bold text-[#33c292] padding-[100px]"}>Anticariat online</h1>
+			  <p className={"w-[1000px] mx-auto text-center mt-3 text-3xl text-[#1e785a]"}>Aplicație web pentru gestionarea cărților dintr-un magazin online</p>
+        <br></br>
+        <br></br>
+        <p className={"container text-left text-2xl mt-8 font-bold text-[#0d3628] pl-[20px]"}>Cărțile disponibile:</p>
+        <br></br>
+        
         <div className="p-4 flex flex-wrap gap-4">
             {data?.map((record) => (
                 <div key={record._id} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{record.denumire}</h5>
+                    <h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">{record.denumire}</h5>
+                    <h5 className="mb-2 font-normal text-gray-800  text-3xl text-[#910a2a] dark:text-gray-600">{record.autor}</h5>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{record.descriere}</p>
+                    <p className="mb-3 font-normal text-gray-700 font-bold text-[#0d3436] text-xl mt-5 font-bold dark:text-gray-400">{record.categorie}</p>
+                    <p className="mb-3 font-normal text-gray-700 text-2xl dark:text-gray-400">{record.stare}</p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{record.pret}</h5>
                     <button type="button"  onClick={()=>handleUpdateRecord(record._id)} className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900">Modifică</button>
                     <button type="button" onClick={()=>handleDeleteRecord(record._id)} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Șterge</button>
                 </div>
             ))}
         </div>
+        </section>
     );
 };
 
